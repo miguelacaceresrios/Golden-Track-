3c10656f707ff31981b683d1f5ca1660fe94aafc
miguelacaceresrios <miguelacaceresrios@gmail.com>
2025-02-10 04:00:50 -0500

    Agregar archivos de proyecto.

---
+++ form1.cs
@@ -0,0 +1,15 @@
+namespace Golden_track
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
